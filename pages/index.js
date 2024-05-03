de6e7bb25937a887f5bebfdd15763bb0ffdf9183
Aubry Nobel Niyonkuru <nobelaubry5@gmail.com>
2024-05-03 16:48:21 -0400

    Page 404 ajouté

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar/Navbar";
 
 export default function Home() {
   return (
-    <div className="mx-auto max-w-sm">
+    <div>
       <h1>Accueil</h1>
     </div>
   );
