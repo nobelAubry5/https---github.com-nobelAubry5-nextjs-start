7e7c1f4c4e03637487dac224e403bbcdf2e15625
Aubry Nobel Niyonkuru <nobelaubry5@gmail.com>
2024-05-04 08:59:49 -0400

    utilité de useStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,34 @@
 import Head from "next/head";
 import Navbar from "../components/Navbar/Navbar";
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props);
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Accueil</title>
       </Head>
-      <h1>Accueil</h1>
+      <h1>Vocabulaire de base</h1>
+      <table className="flex flex-row justify-center">
+        <tbody>
+          {props.array.map((el) => (
+            <tr className="border border-slate-800 text-center">
+              <td className="py-3 px-8 mr-4 border border-slate-800">
+                {el.en}
+              </td>
+              <td className="py-3 px-8 ml-4 border border-slate-800">
+                {el.fr}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
+}
+export async function getStaticProps() {
+  const data = await import(`/data/vocabulary.json`);
+  const array = data.vocabulary;
+  return { props: { array } };
 }
